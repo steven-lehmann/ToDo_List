ebc6db87b7813c4c7541884acd7c3eafc2be0cd9
gosi333 <gowsigan.gowribalan@students.fhnw.ch>
2021-05-27 08:40:05 +0200

    Account, Priority

--- src/ToDo_Client/Account.java
+++ src/ToDo_Client/Account.java
@@ -1,5 +1,49 @@
 package ToDo_Client;
 
+import java.util.ArrayList;
+
 public class Account {
+	
+	private String eMail;
+	private String password;
+	private ArrayList<ToDo>  toDoList;
+	
+	public Account(String eMail, String password, ArrayList<ToDo> toDoList) {
+		this.eMail = eMail;
+		this.password = password;
+		this.toDoList = toDoList;
+		
+		
+	}
+
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public ArrayList<ToDo> getToDoList() {
+		return toDoList;
+	}
+
+	public void setToDoList(ArrayList<ToDo> toDoList) {
+		this.toDoList = toDoList;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [eMail=" + eMail + "]";
+	}
 
 }
--- src/ToDo_Client/Priority.java
+++ src/ToDo_Client/Priority.java
@@ -1,5 +1,15 @@
 package ToDo_Client;
 
 public enum Priority {
+	
+	Low, Medium, Hight;
+	
+	public boolean contains(String searchString) {
+		return (this.name().contains(searchString));
+	}
+	
+	public boolean equals(String searchString) {
+		return (this.name().equals(searchString));
+	}
 
 }
